f45f5d2a80efc1786db58324a874a41e29d70ef2
Kelvin Lam <kelvin.lam@sonydadc.com>
2016-05-25 12:09:39 +1000

    added code and new files

---
+++ inc/template-tags.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Custom template tags for this theme.
+ *
+ * Eventually, some of the functionality here could be replaced by core features
+ *
+ * @package Dev-theme
+ * @since Dev-theme 1.0
+ */
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+    <div id="content" class="site-content" role="main">
+    </div><!-- #content .site-content -->
+</div><!-- #primary .content-area -->
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
